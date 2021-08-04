4af606ec6a9e7d643c5231cc416ed5d36b35d369
Christopher Dent <christopherdent01@gmail.com>
2021-08-03 20:59:51 -0400

    confirmed working on linux

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_02_223634) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "campaigns", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
